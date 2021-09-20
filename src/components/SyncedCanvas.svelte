0c20dd154bc8241518018f11ee1172ab096f3ffb
10c8 <2660859+10c8@users.noreply.github.com>
2021-09-20 02:33:24 +0000

    Update

--- src/components/SyncedCanvas.svelte
+++ src/components/SyncedCanvas.svelte
@@ -111,11 +111,7 @@
 <div class="synced-canvas">
   {#if isEditing}
     <div class="buttons buttons--left">
-      <button
-        title="Desfazer"
-        on:click={onUndo}
-        disabled={drawing.length === 0}
-      >
+      <button title="Desfazer" on:click={onUndo}>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
         </svg>
